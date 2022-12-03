640cbc640c92b319f316d033a5a0d5c0aa237613
Jonas <jonasmagnusson51@gmail.com>
2022-12-03 21:14:24 +0000

    Mostly done, always crashes at 4443rd iteration

--- serverfork.cpp
+++ serverfork.cpp
@@ -23,7 +23,7 @@ void handleMessage(int* clientfd){
 		//printf("Fork entered function\n");
 		char client_message[CAP];
 		memset(client_message, 0, CAP);
-		char buffer2[11000]; // CHANGE THIS TO DYNAMIC
+		char buffer2[10000]; // CHANGE THIS TO DYNAMIC
 		int bytesRecv = 0;
 		char msg[100];
 		memset(msg, 0, 100);
@@ -36,22 +36,22 @@ void handleMessage(int* clientfd){
 			//close(*clientfd);
 			return;
 		}
-		printf("Fork msg: %s\n", client_message);
+		//printf("Fork msg: %s\n", client_message);
 		char buffer3[20] = "HTTP/1.1 200 OK\r\n\r\n";
 		if(write(*clientfd, buffer3, sizeof(buffer3)-1) < 0){
 			printf("Error sending: %s\n", strerror(errno));
 			//close(*clientfd);
 			return;
-		} else printf("Message sent\n");
+		} //else printf("Message sent\n");
 		
 		//Process Message
 		char* command = strtok(client_message, "/");
 		
-		printf("Command: %s\n", command);
+		//printf("Command: %s\n", command);
 		//Check if its a GET or HEAD
 		if(strcmp(command, "GET ") == 0){
 			//If GET, open file and return 
-			printf("Command: \"GET\" recognized\n");
+			//printf("Command: \"GET\" recognized\n");
 			
 			//Get path
 			char* path = strtok(NULL, " ");
@@ -62,17 +62,17 @@ void handleMessage(int* clientfd){
 					nrOf++;
 				}
 			}
-			printf("Path: %s contains %d\n", path, nrOf);
+			//printf("Path: %s contains %d\n", path, nrOf);
 			if(nrOf < 4){
 				//Open filepath
-				printf("Valid path: %s\n", path);
+				//printf("Valid path: %s\n", path);
 				FILE *filePtr;
 				filePtr = fopen(path, "r");
 				if(filePtr == NULL){
 					printf("Failed to open path: %s\n", strerror(errno));
 					exit(0);
 				}
-				else printf("File opened\n");
+				else //printf("File opened\n");
 				//Extract data and store it into the buffer
 				
 				fseek(filePtr, 0, SEEK_SET);
@@ -100,7 +100,7 @@ void handleMessage(int* clientfd){
 		}	
 		//Check version?
 		
-		printf("Returning data\n");
+		//printf("Returning data\n");
 		//Return Data
 		char* finalMessage = (char*)malloc(CAP);
 		char buffer[20] = "HTTP/1.1 200 OK\r\n\r\n";
@@ -112,7 +112,7 @@ void handleMessage(int* clientfd){
 		//} else printf("Message sent\n");
 		if(write(*clientfd, &buffer2, sizeof(buffer2)) < 0){
 			printf("Error sending: %s\n", strerror(errno));
-		} else printf("Message sent: %s\n");
+		} //else printf("Message sent: %s\n");
 
 		//close(*clientfd);
 		
@@ -182,7 +182,6 @@ int main(int argc, char *argv[]){
 		exit(0);
 	} else printf("Server listening\n");
 	
-	//Fork once or to 10 processes?
 	//Fork process (we now have 2 processes)
 	//fork();
 	pid_t my_pid = getpid();
@@ -191,7 +190,7 @@ int main(int argc, char *argv[]){
 	while(true){
 	
 	
-	char buffer2[11000];
+	//char buffer2[11000];
 		//Accept Client
 		char server_message[CAP], client_message[CAP];
 		int bytesRecv;
@@ -201,119 +200,26 @@ int main(int argc, char *argv[]){
 		clientfd = accept(serverfd, (struct sockaddr*)&clientinfo, &len);
 		if(clientfd < 0){
 			printf("Accept error: %s\n", strerror(errno));
-		} printf("Accepted client\n");
+		} //printf("Accepted client\n");
 	
 	
 		//Fork process
 		pid_t forkID = fork();
 		//Check if the process is a child
 		if(forkID == 0){
-			printf("Fork successfull\n");
+			//printf("Fork successfull\n");
 			//close(serverfd);
 			handleMessage(&clientfd);
 			
 		
 		}
-		//OLD CODE IT NEVER RUNS
-		else if(1 == 2){
-		//Read Data
-		char msg[100];
-		memset(msg, 0, 100);
-		bytesRecv = recv(clientfd, client_message, CAP, NULL);
-		if(bytesRecv == 0){
-			//Client dropped
-			printf("Dropping client\n");
-			close(clientfd);
-			continue;
-		}
-		printf("Client msg: %s\n", client_message);
-		char buffer3[20] = "HTTP/1.0 200 OK\r\n\r\n";
-				if(write(clientfd, buffer3, sizeof(buffer3)-1) < 0){
-			printf("Error sending: %s\n", strerror(errno));
-		} else printf("Message sent\n");
-		printf("PID: %d, received message: %s\n", my_pid, client_message);
-	
-		//Process Message
-		char* copy = (char*)malloc(CAP); strcpy(copy, client_message); //Copy message so we dont split the OG message
-		char* command = strtok(copy, "/");
-		
-		//Check if its a GET or HEAD
-		if(strcmp(command, "GET ") == 0){
-			//If GET, open file and return 
-			printf("Command: \"GET\" recognized\n");
-			
-			//Get path
-			//strtok(NULL, "/");
-			char* path = strtok(NULL, " ");
-			//Check if valid path
-			int nrOf = 0;
-			for(int i = 1;i < strlen(path);i++){
-				if(path[i] == '/'){
-					nrOf++;
-				}
-			}
-			printf("Path: %s contains %d\n", path, nrOf);
-			if(nrOf < 4){
-				//Open filepath
-				printf("Valid path: %s\n", path);
-				FILE *filePtr;
-				filePtr = fopen(path, "r");
-				if(filePtr == NULL){
-					printf("Failed to open path: %s\n", strerror(errno));
-					exit(0);
-				}
-				else printf("File opened\n");
-				//Extract data and store it into the buffer
-				//fscanf(filePtr, "%s", server_message);
-				+
-				
-				fseek(filePtr, 0, SEEK_SET);
-				fread(buffer2, sizeof(char), sizeof(buffer2), filePtr);
-				//fscanf(filePtr, "%s", buffer2);
-				fclose(filePtr);
-				
-			}
-			else{
-				//Wrong path. Send error?
-				printf("Path does not exist\n");
-				continue;
-			}
-			
-			
-			
-		}
-		else if(strcmp(command, "HEAD") == 0){
-			//If HEAD, make header?
-		}
-		//Wrong command. Close connection?
-		else{
-			printf("Invalid command: %s\n", command);
-			//close(clientfd);
-			continue;
-		}	
-		//Check version?
-		
-		//Return Data
-		char* finalMessage = (char*)malloc(CAP);
-		char buffer[20] = "HTTP/1.0 200 OK\r\n\r\n";
-		
-		//sprintf(finalMessage, "HTTP/1.1 200 OK\r\n%s\r\n\r\n", server_message);
-		printf("Strlen: %d, Sizeof: %d\n", strlen(buffer2), sizeof(buffer2));
-		//if(send(clientfd, &buffer, sizeof(buffer), 0) < 0){
-			//printf("Error sending: %s\n", strerror(errno));
-		//} else printf("Message sent\n");
-				if(write(clientfd, &buffer2, sizeof(buffer2)) < 0){
-			printf("Error sending: %s\n", strerror(errno));
-		} else printf("Message sent\n");
-
-		close(clientfd);
-		}
-		close(clientfd);
+		else close(clientfd);
 		
 		if(forkID == 0){
 			//kill(forkID, SIGKILL);
+			close(clientfd);
 		}
-		forkID = wait(NULL);
+		//forkID = wait(NULL);
   }
   printf("Done.\n");
   return(0);
